c0c1574aaf9738ce9a28e61b755193115820edb2
Francisca-Heii <franciscahheii@gmail.com>
2023-06-07 15:41:34 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const navIconEl = document.querySelector('.nav--icon');
+const navCloseEl = document.querySelector('.nav--close');
+const navList = document.querySelector('.nav--list');
+const navBgOverlayEl = document.querySelector('.nav--bgOverlay');
+
+// Craeting a function nav open
+const navOpen = () => {
+    navList.classList.add('show');
+    navBgOverlayEl.classList.add('active');
+    document.body.style = 'visibility: visible; height: 100vh; width: 100vw; overflow: hidden;'
+}
+
+// Function to close down nav menu
+const navClose = () => {
+    navList.classList.remove('show');
+    navBgOverlayEl.classList.remove('active');
+    document.body.style ='visibility: visible; height: initial; width: 100%; overflow-x: hidden;'
+}
+
+navIconEl.addEventListener('click', navOpen);
+navCloseEl.addEventListener('click', navClose);
+navBgOverlayEl.addEventListener('click', navClose);
+
+// initializing aos library
+AOS.init({
+    offset: 200,
+    delay: 100,
+    duration: 400,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom',   
+})
